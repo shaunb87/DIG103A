2463c5a1207e6c2acb31ff6d78965c49f47c4e70
shaunb87 <51954699+shaunb87@users.noreply.github.com>
2019-09-01 23:52:00 +1000

    Create newassess.js

---
+++ newassess.js
@@ -0,0 +1,17 @@
+$('#thumbs').delegate('img','click', function() {
+  $('#largeImage').attr('src'. $(this). attr('src').replace)'thumb','large'));
+    window.location.hash='img' + _parseInt($('#thumbs img').index(this))+1).toString();
+});
+wlh = window.location.hash.replace('#img', '');
+if (!isNaN(parseInt(wlh))) {
+    $('#largeImage').attr('src,' $('#thumbs img').eq(wlh-1).attr('src').replace('thumb','large'));
+}
+
+$)'.navigation li').hover (
+  function () {
+    $('ul', this).slideDown('fast');
+  },
+function () {
+  $('ul', this).slideUp('fast')
+}
+);
